dd8f2bb2873feb3280f6133f3d40fd96777cb64d
liz-fw <lstebbins@flowwest.com>
2024-01-18 14:42:08 -0800

    renames clean tables to escapement_estimates

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -8,7 +8,6 @@ library(googleCloudStorageR)
 # TODO project
 # TODO coverage
 
-
 # pull in data from google cloud ---------------------------------------------------
 
 gcs_auth(json_file = Sys.getenv("GCS_AUTH_FILE"))
@@ -32,7 +31,7 @@ gcs_get_object(object_name = "standard-format-data/standard_carcass.csv",
 redd_raw <- read.csv(here::here("data-raw", "standard_daily_redd.csv")) |>
   filter(stream == "yuba river")
 
-up_raw <- read.csv(here::here("data-raw", "standard_adult_passage_estimate.csv")) |>
+escapement_estimates_raw <- read.csv(here::here("data-raw", "standard_adult_passage_estimate.csv")) |>
   filter(stream == "Yuba River")
 
 carcass_raw <- read.csv(here::here("data-raw", "standard_carcass.csv")) |>
@@ -40,16 +39,15 @@ carcass_raw <- read.csv(here::here("data-raw", "standard_carcass.csv")) |>
 
 
 # clean data --------------------------------------------------------------
-# TODO discuss how they will be providing data to us - looking toward the future
 redd <- redd_raw |>
   mutate(date = as.Date(date)) |>
   select(-c(reach, river_mile, fish_guarding, redd_measured, redd_width, redd_length,
-            age, age_index, method, run, year, starting_elevation_ft,
+            age, age_index, method, year, starting_elevation_ft,
             redd_substrate_class, tail_substrate_class, pre_redd_substrate_class)) |> # remove empty columns
   select(-stream) |> # not necessary
   glimpse()
 
-up <- up_raw |>
+escapement_estimates <- escapement_estimates_raw |>
   select(-c(run, sex, viewing_condition, spawning_condition,
             jack_size, flow, temperature, comments)) |>
   select(-stream) |>
@@ -64,13 +62,12 @@ carcass <- carcass_raw |>
 
 # write files -------------------------------------------------------------
 write.csv(redd, here::here("data", "yuba_redd.csv"), row.names = FALSE)
-write.csv(up, here::here("data", "yuba_upstream_passage.csv"), row.names = FALSE)
+write.csv(escapement_estimates, here::here("data", "yuba_escapement_estimates.csv"), row.names = FALSE)
 write.csv(carcass, here::here("data", "yuba_carcass.csv"), row.names = FALSE)
-
 
 # review ------------------------------------------------------------------
 read.csv(here::here("data", "yuba_redd.csv")) |> glimpse()
-read.csv(here::here("data", "yuba_upstream_passage.csv")) |> glimpse()
+read.csv(here::here("data", "yuba_escapement_estimates.csv")) |> glimpse()
 read.csv(here::here("data", "yuba_carcass.csv")) |> glimpse()
 
 
