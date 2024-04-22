bf2855cf14c61c107f0d67e012d82edba0ead18c
liz-fw <lstebbins@flowwest.com>
2024-04-22 15:31:25 -0700

    removes speed column from instantaneous data table and validates xml

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -6,7 +6,7 @@ library(googleCloudStorageR)
 gcs_auth(json_file = Sys.getenv("GCS_AUTH_FILE"))
 gcs_global_bucket(bucket = Sys.getenv("GCS_DEFAULT_BUCKET"))
 
-gcs_get_object(object_name = "adult-upstream-passage-monitoring/yuba-river/data-raw/Yuba VAKI Chinook_QAQC instantaneous.xlsx",
+gcs_get_object(object_name = "adult-upstream-passage-monitoring/yuba-river/data-raw/Yuba VAKI Chinook_QAQC instantaneous_v2.xlsx",
                bucket = gcs_get_global_bucket(),
                saveToDisk = here::here("data-raw", "yuba_instantaneous.xlsx"),
                overwrite = TRUE)
@@ -29,15 +29,14 @@ daily_uncorrected_metadata <- readxl::read_xlsx(here::here("data-raw", "yuba_unc
                                                 skip = 7)
 
 # clean data --------------------------------------------------------------
-# TODO negative speeds
-# TODO each row is a count of 1?
-# TODO unit of speed?
 instant <- instant_raw |>
   janitor::clean_names() |>
   mutate(date = as.Date(date),
          time = hms::as_hms(time),
          time = as.character(time),
          category = str_to_lower(category),
+         ladder = str_to_lower(ladder),
+         direction_of_passage = str_to_lower(direction_of_passage),
          adipose_clipped = case_when(category == "chinook ad-clip" ~ "TRUE",
                                      category == "chinook" ~ "FALSE",
                                      category == "chinook ad-undetermined" ~ "undetermined",
