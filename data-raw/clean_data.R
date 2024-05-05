e4ae3580a50d7912b8142c9175eaf1e6465fd8b6
liz-fw <lstebbins@flowwest.com>
2024-05-05 16:16:50 -0700

    reads in daily corrected passage from paul and processes

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -16,6 +16,11 @@ gcs_get_object(object_name = "adult-upstream-passage-monitoring/yuba-river/data-
                saveToDisk = here::here("data-raw", "yuba_uncorrected_daily.xlsx"),
                overwrite = TRUE)
 
+gcs_get_object(object_name = "adult-upstream-passage-monitoring/yuba-river/data-raw/Yuba VAKI Chinook_Corrected & Run Differentiated Daily Passage Estimates.xlsx",
+               bucket = gcs_get_global_bucket(),
+               saveToDisk = here::here("data-raw", "yuba_corrected_daily.xlsx"),
+               overwrite = TRUE)
+
 instant_raw <- readxl::read_xlsx(here::here("data-raw", "yuba_instantaneous.xlsx"),
                                        sheet = "Yuba VAKI Chinook")
 instant_metadata <- readxl::read_xlsx(here::here("data-raw", "yuba_instantaneous.xlsx"),
@@ -25,6 +30,12 @@ instant_metadata <- readxl::read_xlsx(here::here("data-raw", "yuba_instantaneous
 daily_uncorrected_raw <- readxl::read_xlsx(here::here("data-raw", "yuba_uncorrected_daily.xlsx"),
                                            sheet = "Uncorr CHN net upstream")
 daily_uncorrected_metadata <- readxl::read_xlsx(here::here("data-raw", "yuba_uncorrected_daily.xlsx"),
+                                                sheet = "Metadata",
+                                                skip = 7)
+
+daily_corrected_raw <- readxl::read_xlsx(here::here("data-raw", "yuba_corrected_daily.xlsx"),
+                                         sheet = "YubaCorrected&RunDiffChinook")
+daily_corrected_metadata <- readxl::read_xlsx(here::here("data-raw", "yuba_corrected_daily.xlsx"),
                                                 sheet = "Metadata",
                                                 skip = 7)
 
@@ -59,12 +70,45 @@ daily_uncorrected <- daily_uncorrected_raw |>
   select(date, biological_year, ladder, count, count_type, vaki_operation) |>
   glimpse()
 
+daily_corrected <- daily_corrected_raw |>
+  janitor::clean_names() |>
+  mutate(spring_early_no_ad_clip_chinook = spring_early_all_chinook - spring_early_ad_clip_chinook,
+         spring_late_no_ad_clip_chinook = spring_late_all_chinook - spring_late_ad_clip_chinook,
+         fall_no_ad_clip_chinook = fall_all_chinook - fall_ad_clip_chinook,
+         # there are some biological years where no run differentiation is completed
+         # so here, capture those as "no run" adipose clipped and non-adipose clipped
+         no_run_ad_clip_chinook = ifelse(is.na(spring_early_no_ad_clip_chinook) &
+                                           is.na(spring_late_no_ad_clip_chinook) &
+                                           is.na(fall_no_ad_clip_chinook),
+                                         total_ad_clip_chinook, NA),
+         no_run_no_ad_clip_chinook = ifelse(is.na(spring_early_no_ad_clip_chinook) &
+                                             is.na(spring_late_no_ad_clip_chinook) &
+                                             is.na(fall_no_ad_clip_chinook),
+                                            total_all_chinook - total_ad_clip_chinook,
+                                            NA)) |>
+  select(-c(spring_early_all_chinook, spring_late_all_chinook, fall_all_chinook,
+            total_all_chinook, total_ad_clip_chinook)) |>
+  pivot_longer(spring_early_ad_clip_chinook:no_run_no_ad_clip_chinook,
+               names_to = "count_type",
+               values_to = "count") |>
+  filter(!is.na(count)) |>
+  mutate(run = case_when(str_detect(count_type, "spring_early") ~ "early spring",
+                         str_detect(count_type, "spring_late") ~ "late spring",
+                         str_detect(count_type, "fall") ~ "fall",
+                         str_detect(count_type, "no_run") ~ "no run differentiation",
+                         TRUE ~ NA),
+         adipose_clip = ifelse(str_detect(count_type, "no_ad_clip"), FALSE, TRUE)) |>
+  glimpse()
+
+
 # write files -------------------------------------------------------------
 write.csv(instant, here::here("data", "yuba_instantaneous_passage.csv"), row.names = FALSE)
 write.csv(daily_uncorrected, here::here("data", "yuba_daily_uncorrected_passage.csv"), row.names = FALSE)
+write.csv(daily_corrected, here::here("data", "yuba_daily_corrected_passage.csv"), row.names = FALSE)
 
 # review ------------------------------------------------------------------
 read.csv(here::here("data", "yuba_instantaneous_passage.csv")) |> glimpse()
 read.csv(here::here("data", "yuba_daily_uncorrected_passage.csv")) |> glimpse()
+read.csv(here::here("data", "yuba_daily_corrected_passage.csv")) |> glimpse()
 
 
