c9daac0ae0160f1efa038affbe6d61474de642be
liz-fw <lstebbins@flowwest.com>
2024-05-20 15:32:08 -0700

    adds Poxon and Bratovich 2020 pdf to package

--- data-raw/make_metadata_xml.R
+++ data-raw/make_metadata_xml.R
@@ -4,8 +4,6 @@ library(EMLaide)
 library(readxl)
 library(EML)
 
-# TODO attach Poxon and Bratovich (2020) pdf
-
 datatable_metadata <-
   dplyr::tibble(filepath = c("data/yuba_instantaneous_passage.csv",
                              "data/yuba_daily_uncorrected_passage.csv",
@@ -20,6 +18,16 @@ datatable_metadata <-
                                        c("yuba_instantaneous_passage.csv",
                                          "yuba_daily_uncorrected_passage.csv",
                                          "yuba_daily_corrected_passage.csv")))
+
+# attach Poxon and Bratovich (2020) pdf
+other_entity_metadata <- list("file_name" = "Poxon_and_Bratovich_2020.pdf",
+                              "file_description" = "Methods for Lower Yuba River Chinook Salmon Passage and Run Differentiation Analyses",
+                              "file_type" = "PDF",
+                              "physical" = create_physical("data-raw/metadata/Poxon_and_Bratovich_2020.pdf",
+                                                           data_url = "https://raw.githubusercontent.com/FlowWest/edi-battle-clear-rst/main/data-raw/metadata/Poxon_and_Bratovich_2020.pdf"))
+other_entity_metadata$physical$dataFormat <- list("externallyDefinedFormat" = list("formatName" = "PDF"))
+
+
 # save cleaned data to `data/`
 excel_path <- "data-raw/metadata/yuba_adult_metadata.xlsx"
 sheets <- readxl::excel_sheets(excel_path)
@@ -44,7 +52,8 @@ dataset <- list() |>
   add_maintenance(metadata$maintenance) |>
   add_project(metadata$funding) |>
   add_coverage(metadata$coverage, metadata$taxonomic_coverage) |>
-  add_datatable(datatable_metadata)
+  add_datatable(datatable_metadata) |>
+  add_other_entity(other_entity_metadata)
 
 # GO through and check on all units
 custom_units <- data.frame(id = c("count of fish", "proportion"),
